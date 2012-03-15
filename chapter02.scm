4fb9190cd712af9fc4885d632923d45274181627
p <p>
2012-03-15 13:19:00 +0000

    picture language in biwa scheme added

--- chapter02.scm
+++ chapter02.scm
@@ -447,3 +447,28 @@
   (foldl (lambda(x xs) (cons xs x))
          '()
          seq))
+
+; 2.40
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+(define (prime? n)
+  (fast-prime? n 10))
+
+
