09404b99d4b7755435091ddb311055efebdb0986
pzel <p@manisola.net>
2013-03-01 19:27:38 +0100

    half-adder, adder implemented

--- c33.scm
+++ c33.scm
@@ -276,6 +276,24 @@
     (inverter j output)
     #t))  
 
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    #t))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    #t))
+
+
 (define (logical-not n)
   ((fmap-boolean not) n))
 
