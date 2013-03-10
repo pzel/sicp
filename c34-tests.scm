6589a8c8f27f9974f88a7965a4110897c7dc29f7
pzel <p@manisola.net>
2013-03-10 14:04:09 +0100

    unable to implement parallel-execute :(

--- c34-tests.scm
+++ c34-tests.scm
@@ -1,9 +1,19 @@
 (load "./test.scm")
 (load "./c34.scm")
+(use srfi-1)
 
+(define (churn)
+  (iota 1000000)
+  #t)
+  
 (test 
  '(
-   (=? '#t #t)
-
-
+   (=? '(let ((x 0))
+          (parallel-execute
+           (lambda() (set! x 10))
+           (lambda() (begin (churn) (set! x 50)))
+           (lambda() (set! x 1)))
+          x)
+       1) ; it should equal 50, but I can't acheieve truly parallel execution
+          ; of threads. 
    ))
--- c34.scm
+++ c34.scm
@@ -1,3 +1,5 @@
+(use srfi-18)
+
 ; ex. 3.38
 ; a) Possible results: {35,40,45,50}
 ; b) 80, 55, ...
@@ -7,18 +9,19 @@
 
 
 ; ex. 3.40
-; all possibilities:
-; {100,1000,10 000, 100 000, 1 000 000}
-
-; serialized: {1 000 000}
+; all possibilities: {100, 1000, 10000, 100000, 1000000}
+; serialized:                                  {1000000}
 
 ; ex. 3.41
-; In the current implementation, the destructive operations are serialized and therefore safe, so the balances can be read at anytime. They will reflect the 'current' balance, which may happen to change just after it is read.
+; In the current implementation, the destructive operations are serialized and therefore safe, so the balances can be read at anytime. They will reflect the 'current' balance, which may happen to change right after it is read.
 
 ; ex. 3.42
 ; Yes, the two implementations are the same thing, because they lift the functions to the same serializer instance.
 
 
 
+(define (parallel-execute . procs)
+  (map thread-join!
+       (map thread-start! procs)))
 
-
+  
