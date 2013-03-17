f814f605e0285f3a13237a320fb6fd7fc7bc14de
pzel <p@manisola.net>
2013-03-17 22:17:24 +0100

    s-display, s-take, s-enumerate implemented

--- c35-tests.scm
+++ c35-tests.scm
@@ -3,28 +3,37 @@
 
 (run-tests
  '(
-   (=? 'empty-stream '())
-   (=? '(null-stream? empty-stream) #t)
-   (=? '(stream-car (cons-stream 3 empty-stream)) 3)
-   (=? '(stream-cdr (cons-stream 3 empty-stream)) empty-stream)
-   (=? '(stream-car (stream-cdr (cons-stream 3 (cons-stream 4 empty-stream)))) 4)
+   (=? 'ES '())
+   (=? '(s-null? ES) #t)
+   (=? '(s-car (s-cons 3 ES)) 3)
+   (=? '(s-cdr (s-cons 3 ES)) ES)
+   (=? '(s-car (s-cdr (s-cons 3 (s-cons 4 ES)))) 4)
 
-   (=? '(let ((s1 (cons-stream 10 empty-stream)))
-          (stream-ref s1 0))
+   (=? '(let ((s1 (s-cons 10 ES)))
+          (s-ref s1 0))
        10)
 
-   (=? '(let ((s1 (cons-stream 10 (cons-stream 20 empty-stream))))
-          (stream-ref s1 1))
+   (=? '(let ((s1 (s-cons 10 (s-cons 20 ES))))
+          (s-ref s1 1))
        20)
 
-   (=?s '(let ((s1 (cons-stream 10 (cons-stream 20 empty-stream))))
-           (stream-map s1 (lambda(x) (* x x))))
-        (cons-stream 100 (cons-stream 400 empty-stream)))
+   (=?s '(let ((s1 (s-cons 10 (s-cons 20 ES))))
+           (s-map s1 (lambda(x) (* x x))))
+        (s-cons 100 (s-cons 400 ES)))
 
    (=? '(let ((res (cons 'result '())))
-          (stream-for-each (lambda(x) (set-cdr! res (cons x (cdr res))))
-                           (cons-stream 10 (cons-stream 20 empty-stream)))
+          (s-for-each (lambda(x) (set-cdr! res (cons x (cdr res))))
+                      (s-cons 10 (s-cons 20 ES)))
           res)
         (list 'result 20 10))
+
+   (=?o '(s-display (s-cons 1 (s-cons 2 ES)))
+        "1\n2\n")
+
+   (=?s '(s-take 2 (s-cons 1 (s-cons 2 (s-cons 3 ES))))
+       (s-cons 1 (s-cons 2 ES)))
+
+   (=?s '(s-enumerate-interval 5 8)
+        (s-cons 5 (s-cons 6 (s-cons 7 (s-cons 8 ES)))))
 
    ))
--- c35.scm
+++ c35.scm
@@ -1,31 +1,46 @@
-(define empty-stream '())
-(define null-stream? null?)
 
-(define-syntax cons-stream
+(define-syntax s-cons
   (syntax-rules ()
-    ((cons-stream <val> <str>)
-     (cons <val>
-           (delay <str>)))))
+    ((s-cons <val> <str>)
+     (cons <val> (delay <str>)))))
 
-(define (stream-car s) (car s))
-(define (stream-cdr s) (force (cdr s)))
+(define ES '())
+(define s-null? null?)
+(define (s-car s) (car s))
+(define (s-cdr s) (force (cdr s)))
 
-(define (stream-ref s n)
-  (if (= n 0)
-      (stream-car s)
-      (stream-ref (stream-cdr s)
-                  (- n 1))))
-(define (stream-map s f)
-  (if (null-stream? s)
-      empty-stream
-      (cons-stream (f (stream-car s))
-                   (stream-map (stream-cdr s) f))))
+(define (s-ref s n)
+  (if (<= n 0)
+      (s-car s)
+      (s-ref (s-cdr s)
+             (- n 1))))
 
-(define (stream-for-each f s)
-  (if (null-stream? s)
+(define (s-map s f)
+  (if (s-null? s)
+      ES
+      (s-cons (f (s-car s))
+              (s-map (s-cdr s) f))))
+
+(define (s-for-each f s)
+  (if (s-null? s)
       #t
       (begin 
-        (f (stream-car s))
-        (stream-for-each f (stream-cdr s)))))
-      
+        (f (s-car s))
+        (s-for-each f (s-cdr s)))))
 
+(define (s-display s)
+  (s-for-each (lambda(line) (display line) (newline)) 
+              s))
+
+(define (s-take n s)
+  (if (<= n 0)
+      ES
+      (s-cons (s-car s)
+              (s-take (- n 1) (s-cdr s)))))
+
+      
+(define (s-enumerate-interval low high)
+  (if (> low high)
+      ES
+      (s-cons low
+              (s-enumerate-interval (+ low 1) high))))
